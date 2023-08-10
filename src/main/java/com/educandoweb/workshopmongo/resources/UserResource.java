24ba65687e1bec4b72589e080c8de48346940e1b
GabrielDeNobrega <sr.gabrielnobrega@hotmail.com>
2023-08-10 17:03:13 -0300

    [WSB3MDB-07] User delete.

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -48,4 +48,11 @@ public class UserResource {
 
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
+
 }
