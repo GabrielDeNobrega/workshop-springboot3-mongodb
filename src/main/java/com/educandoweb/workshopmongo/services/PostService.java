907a44fed87ca435fb5dcdba6f4cf1c925c89c13
GabrielDeNobrega <sr.gabrielnobrega@hotmail.com>
2023-08-11 11:50:00 -0300

    [WSB3MDB-13] Finding a post by its id.

---
+++ src/main/java/com/educandoweb/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.educandoweb.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.workshopmongo.domain.Post;
+import com.educandoweb.workshopmongo.repositories.PostRepository;
+import com.educandoweb.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+	}
+
+}
